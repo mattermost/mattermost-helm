8ea09c0917f4eb1f02fd51356a8d4e1009464aff
Khosrow Moossavi <khos2ow@gmail.com>
2021-01-28 08:59:11 -0500

    Update helm version to v3.4.1

--- .circleci/install_tools.sh
+++ .circleci/install_tools.sh
@@ -2,16 +2,15 @@
 
 set -o errexit
 
-readonly HELM_VERSION=2.16.7
+readonly HELM_VERSION=3.4.1
 readonly CHART_RELEASER_VERSION=1.0.0-beta.1
 
 echo "Installing Helm..."
-curl -LO "https://kubernetes-helm.storage.googleapis.com/helm-v$HELM_VERSION-linux-amd64.tar.gz"
+curl -LO "https://get.helm.sh/helm-v$HELM_VERSION-linux-amd64.tar.gz"
 sudo mkdir -p "/usr/local/helm-v$HELM_VERSION"
 sudo tar -xzf "helm-v$HELM_VERSION-linux-amd64.tar.gz" -C "/usr/local/helm-v$HELM_VERSION"
 sudo ln -s "/usr/local/helm-v$HELM_VERSION/linux-amd64/helm" /usr/local/bin/helm
 rm -f "helm-v$HELM_VERSION-linux-amd64.tar.gz"
-helm init --client-only
 
 echo "Installing chart-releaser..."
 curl -LO "https://github.com/helm/chart-releaser/releases/download/v${CHART_RELEASER_VERSION}/chart-releaser_${CHART_RELEASER_VERSION}_linux_amd64.tar.gz"
